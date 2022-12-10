613246c2627036f4cdb80621de8aae9d98668734
Neo Fu <rtugeek@gmail.com>
2022-12-10 17:17:12 +0800

    + BrowserWindowApi.openUrl

--- src/api/BrowserWindowApi.ts
+++ src/api/BrowserWindowApi.ts
@@ -3,9 +3,11 @@ import {ElectronUtils} from "../utils/ElectronUtils";
 
 export class BrowserWindowApi {
 
-    static readonly  IGNORE_MOUSE_EVENT = "ignore-mouse-event"
-    static readonly  WINDOW_VISIBILITY = "window-visibility"
-    static readonly  ALWAYS_ON_TOP = "always-on-top"
+    static readonly IGNORE_MOUSE_EVENT = "ignore-mouse-event"
+    static readonly WINDOW_VISIBILITY = "window-visibility"
+    static readonly ALWAYS_ON_TOP = "always-on-top"
+    static readonly OPEN_LINK = "open-link"
+
     static async setIgnoreMouseEvent(ignore: boolean) {
         await ElectronUtils.getAPI().invoke(Channel.BROWSER_WINDOW, this.IGNORE_MOUSE_EVENT, ignore);
     }
@@ -16,6 +18,11 @@ export class BrowserWindowApi {
 
     static async setAlwaysOnTop(alwaysOnTop: boolean) {
         await ElectronUtils.getAPI().invoke(Channel.BROWSER_WINDOW, this.ALWAYS_ON_TOP, alwaysOnTop);
+    }
+
+
+    static async openUrl(url: string) {
+        await ElectronUtils.getAPI().invoke(Channel.BROWSER_WINDOW, this.OPEN_LINK, url);
     }
 
 }
